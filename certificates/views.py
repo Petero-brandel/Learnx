a1196529229563f70ece6ee1b3c978b019a5469c
petero-brandel <peter01rebrandel@gmail.com>
2026-05-16 00:43:34 -0700

    Add on-the-fly certificate download endpoint

--- certificates/views.py
+++ certificates/views.py
@@ -1,7 +1,11 @@
 from rest_framework import views, status
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from django.http import HttpResponse
 from .models import Certificate
+from .generator import create_certificate_pdf
+from datetime import datetime
+
 
 class MyCertificatesView(views.APIView):
     permission_classes = [IsAuthenticated]
@@ -12,10 +16,33 @@ class MyCertificatesView(views.APIView):
         data = []
         for cert in certificates:
             data.append({
-                'certificate_id': cert.certificate_id,
+                'certificate_id': str(cert.certificate_id),
                 'course_title': cert.course.title,
                 'issued_at': cert.issued_at,
-                'pdf_url': cert.pdf_file.url if cert.pdf_file else None
             })
             
         return Response(data, status=status.HTTP_200_OK)
+
+
+class CertificateDownloadView(views.APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, certificate_id, *args, **kwargs):
+        try:
+            cert = Certificate.objects.select_related('course', 'user').get(
+                certificate_id=certificate_id,
+                user=request.user
+            )
+        except Certificate.DoesNotExist:
+            return Response({'error': 'Certificate not found'}, status=status.HTTP_404_NOT_FOUND)
+
+        # Generate PDF on the fly
+        student_name = cert.user.get_full_name() or cert.user.email
+        date_str = cert.issued_at.strftime("%B %d, %Y")
+        pdf_content = create_certificate_pdf(student_name, cert.course.title, date_str, cert.certificate_id)
+
+        # Stream as downloadable PDF
+        response = HttpResponse(pdf_content.read(), content_type='application/pdf')
+        safe_title = cert.course.title.replace(' ', '_').replace('/', '-')
+        response['Content-Disposition'] = f'attachment; filename="Certificate_{safe_title}.pdf"'
+        return response
